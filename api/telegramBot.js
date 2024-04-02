b75305e0575fc01e28cda9e6ee0878d8ddbe8730
Divya Ranjan <divya.ran7an@gmail.com>
2024-04-03 00:56:44 +0530

    Update

--- api/telegramBot.js
+++ api/telegramBot.js
@@ -5,7 +5,7 @@ module.exports = async (req, res) => {
     if (message && message.text) {
         const chatId = message.chat.id;
         const replyText = `Echo: ${message.text}`;
-        await axios.post(`https://api.telegram.org/bot${process.env.TELEGRAM_BOT_TOKEN}/sendMessage`, {
+        await axios.post(`https://api.telegram.org/bot6764939618:AAGJ8XSHH6N7yShX4SoIF0eFj9c7yZ4iyqY/sendMessage`, {
             chat_id: chatId,
             text: replyText,
         });
