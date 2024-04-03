db2f2afc1c754efbaa20fc7bce0647c83ff96798
Divya Ranjan <divya.ran7an@gmail.com>
2024-04-03 19:34:25 +0530

    Update

--- api/convert.js
+++ api/convert.js
@@ -1,6 +1,6 @@
 const fetch = require('node-fetch');
 
-// TranscribeTube endpoint template
+// Assuming you're using the TranscribeTube API
 const TRANSCRIBETUBE_API_URL = 'https://tapi.transcribetube.com/api/v1/transcript/{videoId}?apikey=4e6MWJXqCx@oQ5?pPEhLDPO%jLedCkPdaxMPh&jAMbV5#uFy';
 
 async function getVideoTranscription(videoId) {
@@ -20,6 +20,7 @@ async function getVideoTranscription(videoId) {
 }
 
 module.exports = async (req, res) => {
+  // Extract videoId from query parameters
   const { videoId } = req.query;
 
   try {
